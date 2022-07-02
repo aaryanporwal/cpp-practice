0eddea8e33b0a46078f5cf0aba8b873afec05f93
Aaryan Porwal <aaryanporwal2233@gmail.com>
2022-07-02 12:00:46 -0400

    Added palindrome

--- Recursion/palindrome.cpp
+++ Recursion/palindrome.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
+// i/p: abba
+// o/p: True
 
-int main(){
-  
+bool isPalindrome(string &str, int start, int end) {
+  if (start >= end) {
+    return true;
+  } else {
+    return (str[start] == str[end] && isPalindrome(str, start + 1, end - 1));
+  }
+}
+
+int main() {
+  string str = "TACOCAT";
+  isPalindrome(str, 0, str.length() - 1) ? cout << "Is Palindrome"
+                                         : cout << "Isn't Palindrome";
+  return 0;
 }
